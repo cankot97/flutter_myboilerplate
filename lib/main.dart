11af6d007fd4d8eca18c30b79b5e73c90e8fb852
cankot97 <mngo@deakin.edu.au>
2021-12-17 09:21:27 +1100

    draft3_setup_file_structure

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,6 @@ import 'package:provider/provider.dart';
 import 'package:secondapp/services/auth_service.dart';
 
 
-
 Future<BodyPart> getBpdescr(int i) async {
   var dio = Dio();
   var res = await dio.get('https://jsonplaceholder.typicode.com/albums/' + i.toString());
@@ -49,134 +48,8 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  
-  final authenticator = AuthService();
-  double? markerX ;
-  double? markerY ;
-
-  BodyPart? whatBodyPart(double? x, double? y){
-    for(var bodypart in bodyparts){
-      if(bodypart.isBodyPart(x, y)){
-        return bodypart;
-      }
-    }
-    return null;
-  }
-
-  void _onTapDown(TapDownDetails details){
-    double x = details.globalPosition.dx;
-    double y = details.globalPosition.dy;  
-
-    setState(() {
-      markerX = x;
-      markerY = y - AppBar().preferredSize.height - MediaQuery.of(context).padding.top;
-    });
-
-    showModalBottomSheet(
-      context: context,
-      builder: (BuildContext builder){
-        return Container(
-          child: detailCard(whatBodyPart(markerX, markerY)),
-        );
-      }
-    );
-  }
 
 
-
-  @override
-  Widget build(BuildContext context) {
-    
-    if (FirebaseAuth.instance.currentUser == null) {return const  Authwrapper();}
-    return Scaffold(
-      appBar: AppBar(
-
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Stack(
-          children:  [
-            GestureDetector(
-              child: Container(
-                height: MediaQuery.of(context).size.height,
-                child: Image.asset(
-                  'assets/images/body.jpeg',
-                  fit: BoxFit.cover,
-                ),
-              ),
-              onTapDown: (TapDownDetails details) =>  _onTapDown(details),
-            ),
-            Positioned(
-              left: markerX, 
-              top: markerY,
-              child: Container(
-                width: 10,
-                height: 10,
-                decoration: const BoxDecoration(
-                  shape: BoxShape.circle,
-                  color: Colors.red,
-                ),
-              ),
-            ),
-            ElevatedButton(
-              child: const Text('Log out'),
-              onPressed: () async {
-                await authenticator.signOut();
-              },
-            )
-
-          ])
-      ),
-    );
-  }
-
-  Widget detailCard(BodyPart? bp){
-    return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        children: [
-          Text(
-            bp?.name ?? 'No Body Part',
-            style: const TextStyle(
-              fontWeight: FontWeight.w400,
-              fontSize: 30
-            ),
-          ),
-          FutureBuilder<BodyPart>(
-            future: getBpdescr(bp?.id ?? 10),
-            builder: (context, snapshot) {
-              if(snapshot.hasData) {
-                return Text(snapshot.data?.title ?? "Fetching Error");
-              }
-              return const CircularProgressIndicator();
-            },
-          ),
-          ElevatedButton(
-            child: const Text('Back'),
-            onPressed: () {
-              Navigator.pop(context);
-            },
-          )
-        ],
-      ),
-    );
-  }
-
-}
-// intergate firebase user authentication.
-// login and regis screen
-//screen for login user information
-
-//packages provider
 
 
 
--- lib/screens/Auth/auth_wrapper.dart
+++ lib/screens/Auth/auth_wrapper.dart
@@ -2,8 +2,8 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:secondapp/screens/Auth/login_screen.dart';
-import 'package:secondapp/main.dart';
 import 'package:provider/provider.dart';
+import 'package:secondapp/screens/home_screen.dart';
 
 class Authwrapper extends StatefulWidget {
   const Authwrapper({ Key? key }) : super(key: key);
